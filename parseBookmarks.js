fc1aef612a9623976d3959cb7d392dfdda860357
Mario Rajchikj <mario92mk@gmail.com>
2018-06-26 15:53:04 +0200

    improvements and promisifying functions

--- parseBookmarks.js
+++ parseBookmarks.js
@@ -1,34 +1,53 @@
 const fs = require('fs')
 const cheerio = require('cheerio')
-const util = require('util')
-const readFile = util.promisify(fs.readFile)
 
 async function parseBookmarks() {
-	let temp = await readFile('bookmarks.html', 'utf-8').catch(err => Promise.resolve(err));
-	let $ = await cheerio.load(temp)
-	let result = await []
+	/*
+	* type: Object
+	* error: Boolean
+	* errorCode: Error/undefined
+	* data: undefined/fileData
+	*/
+	let temp = await readFile()
+	if (temp.error) {
+		console.log(temp.errorCode)
+		return;
+	} else {
+		let $ = await cheerio.load(temp.data)
+		let result = await []
 
-	$('a').each(function (index, a) {
-		let $a = $(a)
-		let categories = getCategories($a)
-		let data = {
-			'title': $a.text(),
-			'url': $a.attr('href'),
-			'categories': categories,
-		}
-		result.push(data)
-	})
-	await console.log(JSON.stringify(result, null, 4))
+		$('a').each((index, a) => {
+			let $a = $(a)
+			let categories = getCategories($a)
+			let data = {
+				title: $a.text(),
+				url: $a.attr('href'),
+				categories
+			}
+			result.push(data)
+		})
+		await console.log(JSON.stringify(result, null, 4))
+	}
 }
 
-function getCategories($a) {
-	let $node = $a.closest('DL').prev();
-	let title = $node.text();
-	if ($node.length > 0 && title.length > 0) {
-		return title = [title].concat(getCategories($node));
-	} else {
-		return [];
-	}
+// let parseLinks
+
+let getCategories = ($a) => {
+		let $node = $a.closest('DL').prev();
+		let title = $node.text();
+		if ($node.length > 0 && title.length > 0) {
+			return title = [title].concat(getCategories($node))
+		} else {
+			return [];
+		}
+}
+
+let readFile = () => {
+	return new Promise((resolve, reject) => {
+		fs.readFile('bookmarks.html', 'utf-8', (err, data) => {
+			(err) ? resolve({ error: true, errorCode: err }) : resolve({ error: false, data })
+		})
+	})
 }
 
 parseBookmarks();
