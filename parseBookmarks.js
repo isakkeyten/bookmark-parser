e923b169467029d914cde6df2e2d14355b2be707
Mario Rajcik <mario92mk@gmail.com>
2018-06-28 00:01:23 +0200

    signed ssh key and eslint

--- parseBookmarks.js
+++ parseBookmarks.js
@@ -1,52 +1,52 @@
-const fs = require('fs')
-const cheerio = require('cheerio')
+const fs = require('fs');
+const cheerio = require('cheerio');
 
 async function parseBookmarks() {
-	let temp = await readFile()
+	let temp = await readFile();
 	if (temp.error) {
 		// return(temp.errorCode)
-		console.log(temp.errorCode)
+		console.log(temp.errorCode);
 	} else {
-		let $ = await cheerio.load(temp.data)
-		let result = await parseLinks($)
+		let $ = await cheerio.load(temp.data);
+		let result = await parseLinks($);
 		// return result	
-		await console.log(JSON.stringify(result, null, 4))
+		await console.log(JSON.stringify(result, null, 4));
 	}
 }
 
 let parseLinks = ($) => {
-	return new Promise((resolve, reject) => {
-		let result = []
+	return new Promise((resolve) => {
+		let result = [];
 		$('a').each((index, a) => {
-			let $a = $(a)
-			let categories = getCategories($a)
+			let $a = $(a);
+			let categories = getCategories($a);
 			let data = {
 				title: $a.text(),
 				url: $a.attr('href'),
 				categories
-			}
-			result.push(data)
-		})
-		resolve(result)
-	})
-}
+			};
+			result.push(data);
+		});
+		resolve(result);
+	});
+};
 
 let getCategories = ($a) => {
-		let $node = $a.closest('DL').prev();
-		let title = $node.text();
-		if ($node.length > 0 && title.length > 0) {
-			return title = [title].concat(getCategories($node))
-		} else {
-			return [];
-		}
-}
+	let $node = $a.closest('DL').prev();
+	let title = $node.text();
+	if ($node.length > 0 && title.length > 0) {
+		return title = [title].concat(getCategories($node));
+	} else {
+		return [];
+	}
+};
 
 let readFile = () => {
-	return new Promise((resolve, reject) => {
+	return new Promise((resolve) => {
 		fs.readFile('bookmarks.html', 'utf-8', (err, data) => {
-			(err) ? resolve({ error: true, errorCode: err }) : resolve({ error: false, data })
-		})
-	})
-}
+			(err) ? resolve({ error: true, errorCode: err }) : resolve({ error: false, data });
+		});
+	});
+};
 
 parseBookmarks();
